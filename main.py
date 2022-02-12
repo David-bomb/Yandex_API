156dfc4d3df83c4ff7f66c709ff7f45962c80bb0
David <kot.shubin@mail.ru>
2022-02-12 19:34:05 +0300

    задача 1

--- main.py
+++ main.py
@@ -4,25 +4,37 @@ import sys
 import requests
 from PyQt5.QtGui import QPixmap
 from PyQt5 import uic
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel
+from PyQt5.QtWidgets import QApplication, QMainWindow
 
 SCREEN_SIZE = [600, 450]
 
 
-class Example(QWidget):
+class Example(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi("main.ui", self)
-        self.setWindowTitle('Кафты')
         self.setFixedSize(*SCREEN_SIZE)
+        self.getImage()
+        self.initUI()
+        self.setWindowTitle('Кафты')
 
     def getImage(self):
-        map_request = "http://static-maps.yandex.ru/1.x/?ll=37.530887,55.703118&spn=0.002,0.002&l=map"
-        response = requests.get(map_request)
+        api_server = "http://static-maps.yandex.ru/1.x/"
+
+        lon = "37.618879"
+        lat = "55.751426"
+        delta = "0.002"
+
+        params = {
+            "ll": ",".join([lon, lat]),
+            "spn": ",".join([delta, delta]),
+            "l": "map"
+        }
+        response = requests.get(api_server, params=params)
 
         if not response:
             print("Ошибка выполнения запроса:")
-            print(map_request)
+            print(response)
             print("Http статус:", response.status_code, "(", response.reason, ")")
             sys.exit(1)
 
@@ -32,11 +44,9 @@ class Example(QWidget):
             file.write(response.content)
 
     def initUI(self):
-        self.setGeometry(100, 100, *SCREEN_SIZE)
-        self.setWindowTitle('Отображение карты')
-
         ## Изображение
         self.pixmap = QPixmap(self.map_file)
+        self.image.setGeometry(0, 0, *SCREEN_SIZE)
         self.image.setPixmap(self.pixmap)
 
     def closeEvent(self, event):
